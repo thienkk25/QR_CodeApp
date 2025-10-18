d2acc4e933b07dd0cef1c1d2209ded96a1a74566
thienkk25 <thiennguyendz25@gmail.com>
2025-10-18 11:28:30 +0700

    change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,14 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystorePropertiesFile = file("C:/Users/thien/develop/SigningConfigs/key.properties")
+val keystoreProperties = Properties()
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -6,7 +17,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.qr_code_app"
+    namespace = "com.thien.qr_code_app"
     compileSdk = 35
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +32,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.qr_code_app"
+        applicationId = "com.thien.qr_code_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -30,13 +41,26 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+    signingConfigs {
+        create("release") {
+            if (keystorePropertiesFile.exists()) {
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
         }
     }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
 }
 
 flutter {
--- android/app/src/main/kotlin/com/example/qr_code_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/qr_code_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.qr_code_app
+package com.thien.qr_code_app
 
 import io.flutter.embedding.android.FlutterActivity
 
